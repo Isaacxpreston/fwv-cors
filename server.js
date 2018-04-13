ec95e5f99293d1ed191e7b098b5c940dd1fd20cb
Isaac <isaacxpreston@gmail.com>
2018-04-13 15:50:58 -0400

    updates

--- server.js
+++ server.js
@@ -14,9 +14,25 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // reference: https://medium.com/trisfera/using-cors-in-express-cac7e29b005b
+var allowedOrigins = [
+  'https://futures-test-cors.herokuapp.com',
+  'https://futures-without-violence-vpv.herokuapp.com',
+  'https://changingmindsnow.org'
+];
 app.use(cors({
   credentials: true,
-}))
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const auth_route = require('./routes/auth.js')
 const surveyRoute = require('./routes/survey.js')
